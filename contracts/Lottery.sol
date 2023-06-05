3261e238a3a0f2d207b759b710d28a44451e6573
F Hersi <33755306+fhersi@users.noreply.github.com>
2023-06-05 12:28:26 +0100

    add openBets func along with unit tests

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,21 +1,73 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {LotteryToken} from "./LotteryToken.sol";
 
 contract Lottery is Ownable {
-/// @notice flag indicating whether the lottery is open for betting
-bool public betsOpen;
+    /// @notice Address of the ERC20 token used for payments
+    LotteryToken public paymentToken;
+    /// @notice flag indicating whether the lottery is open for betting
+    bool public betsOpen;
+    /// @notice timestamp of the closing time of the current lottery
+    uint256 public betsClosingTime;
+    /// @notice Ratio determining the ration of LotteryTokens to Eth
+    uint256 tokenRatio;
+    /// @notice the cost of each bet - paid to the prizePool
+    uint256 betPrice;
+    /// @notice the fee associated with each bet - paid to the ownerPool;
+    uint256 betFee;
+    /// @notice the total amount winnable for the current lottery
+    uint256 prizePool;
+    /// @notice the total fees collected from each betFee
+    uint256 ownerPool;
 
-constructor() {}
+    /// @notice mapping of each winners address and prizeAmount.
+    /// @dev allows further lotterys to run without state being cleared.
+    mapping(address => uint256) winnersPool;
 
-modifier whenBetsClosed() {
-    require(!betsOpen);
-    _;
-}
- 
-function openBets(uint8 _targetEndTime) external onlyOwner whenBetsClosed {
+    /// @notice Array of participants in current running lottery.
+    /// @dev This is wiped after each lottery ends.
+    address[] _slots;
 
-}
+    constructor(
+        string memory tokenName,
+        string memory tokenSymbol,
+        uint256 _tokenRatio,
+        uint256 _betPrice,
+        uint256 _betFee
+    ) {
+        paymentToken = new LotteryToken(tokenName, tokenSymbol);
+        tokenRatio = _tokenRatio;
+        betPrice = _betPrice;
+        betFee = _betFee;
+    }
 
+    /// @notice modifier to ensure that there are no running lotterys
+    /// @dev checks flag to ensure no lotterys are live
+    modifier whenBetsClosed() {
+        require(!betsOpen, "Lottery is open");
+        _;
+    }
+
+    /// @notice modifier to ensure that there is a live lottery in progress
+    /// @dev checks whether bets are open as well as
+    modifier whenBetsOpen() {
+        require(
+            betsOpen && block.timestamp < betsClosingTime,
+            "Lottery is closed"
+        );
+        _;
+    }
+
+    function openBets(
+        uint256 _targetEndTime
+    ) external onlyOwner whenBetsClosed {
+        require(
+            _targetEndTime > block.timestamp,
+            "End time must be in the future"
+        );
+        betsClosingTime = _targetEndTime;
+        betsOpen = true;
+    }
 }
--- tests/Test.ts
+++ tests/Test.ts
@@ -1,40 +1,126 @@
-describe("deployment", function () {
-  it("Should Implement ownable", async () => {
-    throw new Error("Not implemented");
-  });
-  it("Should Only Allow The Owner To deploy lottery and define betting price and fee", async () => {
-    throw new Error("Not implemented");
-  });
-  it("Should Require That Only Owner start lottery", async () => {
-    it("Should require a block timestamp target", async () => {
-      throw new Error("Not implemented");
-    });
-  });
-});
+import { expect, should } from "chai";
+import { ContractFactory } from "ethers";
+import { ethers } from "hardhat";
+import { Lottery, LotteryToken, Lottery__factory } from "../typechain-types";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Block } from "@ethersproject/providers";
 
-describe("Outside Of Betting Window", function () {
-  it("Players must buy an ERC20 with ETH", async () => {
-    throw new Error("Not implemented");
+describe("Lottery Dapp", async () => {
+  const TOKEN_RATIO = 1;
+  const BET_PRICE = 0.8;
+  const BET_FEE = 0.2;
+
+  let contract: Lottery;
+  let token: LotteryToken;
+  let accounts: SignerWithAddress[];
+  let currentTimestamp: number;
+
+  beforeEach(async () => {
+    accounts = await ethers.getSigners();
+    const contractFactory = new Lottery__factory(accounts[0]);
+    contract = await contractFactory.deploy(
+      "Lottery Token",
+      "LT0",
+      TOKEN_RATIO,
+      ethers.utils.parseEther(BET_PRICE.toFixed(18)),
+      ethers.utils.parseEther(BET_FEE.toFixed(18))
+    );
   });
-  it("Players pay ERC20 to bet", async () => {
-    it("Only possible before block timestamp met", async () => {
+
+  describe("development", function () {
+    beforeEach(async () => {
+      currentTimestamp = (await ethers.provider.getBlock("latest")).timestamp;
+    });
+    describe("open bets", async () => {
+      it("ensures that only owner can open bets", async () => {
+        await expect(
+          contract
+            .connect(accounts[1])
+            .openBets(currentTimestamp + Number(1 * 60))
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+      });
+      it("ensures there is no other running lottery", async () => {
+        await contract.openBets(currentTimestamp + Number(1 * 60));
+        let flag: boolean = await contract.betsOpen();
+        if (flag)
+          await expect(
+            contract.openBets(currentTimestamp + Number(1 * 60))
+          ).to.be.revertedWith("Lottery is open");
+      });
+      it("end timestamp cannot be in the past", async () => {
+        await expect(
+          contract.openBets(currentTimestamp - Number(1 * 60))
+        ).to.be.revertedWith("End time must be in the future");
+      });
+      it("updates betClosing time variable", async () => {
+        // starting a lottery should update the betsClosingTime timestamp to not be 0;
+        await contract.openBets(currentTimestamp + Number(1 * 60));
+        let closingTime = await contract.betsClosingTime();
+        await expect(closingTime).to.be.gt(0);
+      });
+      it("updates betsOpen flag", async () => {
+        // starting a lottery should cause the betsOpen flag to evaluate to true.
+        await contract.openBets(currentTimestamp + Number(1 * 60));
+        let flag = await contract.betsOpen();
+        await expect(flag).to.be.eq(true);
+      });
+    });
+
+    describe("Purchase Tokens", function () {
+      it("purchase token", async () => {});
+    });
+
+    it("bet", async () => {});
+    it("Bet many", async () => {});
+    it("closeLottery", async () => {});
+    it("getRando", async () => {});
+    it("prize withdraw??", async () => {});
+    it("ownerWithdraw", async () => {});
+    it("returnTokens", async () => {});
+  });
+
+  describe("deployment", function () {
+    it("Should Implement ownable", async () => {
+      // transferOwnership / renounceOwnership
       throw new Error("Not implemented");
     });
+    it("Should Only Allow The Owner To deploy lottery and define betting price and fee", async () => {
+      //'Ownable: caller is not the owner',
+      throw new Error("Not implemented");
+    });
+    it("Should Require That Only Owner start lottery", async () => {
+      // "Ownable: caller is not the owner",
+      it("Should require a block timestamp target", async () => {
+        throw new Error("Not implemented");
+      });
+    });
   });
-  it("Anyone can roll the lottery", async () => {
-    it("Only after block timestamp target is met", async () => {});
-    it("Randomness from RANDAO", async () => {});
+
+  describe("Outside Of Betting Window", function () {
+    it("Players must buy an ERC20 with ETH", async () => {
+      throw new Error("Not implemented");
+    });
+    it("Players pay ERC20 to bet", async () => {
+      it("Only possible before block timestamp met", async () => {
+        throw new Error("Not implemented");
+      });
+    });
+    it("Anyone can roll the lottery", async () => {
+      it("Only after block timestamp target is met", async () => {});
+      it("Randomness from RANDAO", async () => {});
+    });
   });
-});
-describe("During Betting Window", function () {});
-describe("After Completion of Lottery", function () {
-  it("Winner receives the pooled ERC20 minus fee", async () => {
-    throw new Error("Not implemented");
-  });
-  it("Owner can withdraw fees and restart lottery", async () => {
-    throw new Error("Not implemented");
-  });
-  it("Players can burn ERC20 tokens and redeem ETH", async () => {
-    throw new Error("Not implemented");
+
+  describe("During Betting Window", function () {});
+  describe("After Completion of Lottery", function () {
+    it("Winner receives the pooled ERC20 minus fee", async () => {
+      throw new Error("Not implemented");
+    });
+    it("Owner can withdraw fees and restart lottery", async () => {
+      throw new Error("Not implemented");
+    });
+    it("Players can burn ERC20 tokens and redeem ETH", async () => {
+      throw new Error("Not implemented");
+    });
   });
 });
